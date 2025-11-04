61f03b3b0a974c7f37259946f3ba5acee6bdbb9e
Shreshtha Srivastava <srivastavashreshtha1@gmail.com>
2025-11-04 23:02:43 +0530

    designed sidebar

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   const {setTheme}=useTheme()
   return (
     <div>
-      <h2>hii i am arya</h2>
+      <h2>New Chat</h2>
       <Button onClick={()=>setTheme('dark')}>Dark Mode</Button>
       <Button onClick={()=>setTheme('light')}>Light Mode</Button>
     </div>
